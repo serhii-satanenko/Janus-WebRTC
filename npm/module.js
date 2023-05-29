f381a3d81403e30f0fbf956f6f7269a9ab2d455f
Serhii Satanenko <64694864+serhii-satanenko@users.noreply.github.com>
2023-05-29 20:13:57 +0300

    init

---
+++ npm/module.js
@@ -0,0 +1,10 @@
+to_remove: {
+/*
+ * Module shim for rollup.js to work with.
+ * Simply re-export Janus from janus.js, the real 'magic' is in the rollup config.
+ */
+}
+
+@JANUS_CODE@
+
+export default Janus;
